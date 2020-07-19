735aea84598ac451994b68e143dc9354e6948388
kosehy@gmail.com <kosehy@gmail.com>
2020-07-19 05:07:31 +0000

    finish todo list

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+// app/javascript/packs/application.js
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -15,3 +17,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+require("./components/TodoApp");
+require('bootstrap')
+import 'bootstrap/dist/css/bootstrap'
